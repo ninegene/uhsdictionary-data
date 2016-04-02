dc91df629b406dd802b8f49b3f3f6168d32e1535
aung <aung@macdev.local>
2016-04-02 00:42:33 -0700

    improve cvs2json and json output to array of objects

---
+++ validate-converted-json.js
@@ -0,0 +1,7 @@
+console.log(require('./data/pali_dict.sqlite.unicode.json'));
+
+
+// var fs = require('fs');
+// fs.readFile('./data/pali_dict.sqlite.unicode.json', 'utf8', function (err, data) {
+//     console.log(JSON.parse(data));
+// });
